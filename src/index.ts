ab8fa771f91442fa7f6e325e0991a03481eedde0
ponnupkr-beep <ponnu.pkr@gmail.com>
2026-04-04 01:54:58 -0500

    Change root route to serve index.html

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 10000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'VaporSense AI is running! 🚀' });
+res.sendFile(path.join(__dirname, 'index.html'));  
 });
 
 app.listen(PORT, () => {
