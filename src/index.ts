a40559b6e23329e85412a27eeff241520a614461
ponnupkr-beep <ponnu.pkr@gmail.com>
2026-04-04 00:57:25 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
